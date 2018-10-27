bb601a0a68c17b15100009b3a0618cf93c770980
Wojciech Malicki <35421818+WojciechMalicki@users.noreply.github.com>
2018-10-27 11:23:22 +0200

    literówka

--- Super strategia.cpp
+++ Super strategia.cpp
@@ -66,7 +66,7 @@ int main()
 		cout<<"Domy:" << domy2 <<endl;
 		cout<<"Farmy:" << farmy2 << endl;
 		cout<<"Sklepy:" << sklepy2 << endl;
-		cout<<"Bazy wojakowe:" << bazy_wojskowe2 <<endl;
+		cout<<"Bazy wojskowe:" << bazy_wojskowe2 <<endl;
 		cout<<"Ludzie:" <<ludzie2 << endl;
 		cout<<"Zolnierze:" <<zolnierze2 <<endl;
 		SetConsoleTextAttribute( hOut, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_INTENSITY );
